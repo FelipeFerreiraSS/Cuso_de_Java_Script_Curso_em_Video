befd1294f10d6cb79e30bc6c4748827f080bcdf4
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-10-29 17:30:53 -0300

    EX: Verificar idade 02

--- Java_Script_Curso_em_video/aula12ex/ex015/script.js
+++ Java_Script_Curso_em_video/aula12ex/ex015/script.js
@@ -6,6 +6,44 @@ function verificar() {
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('erro')
     } else {
-        alert('ok')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homen'
+            if (idade >= 0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'bebe_h.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovem_h.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'adulto_h.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso_h.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'bebe_m.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovem_m.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'adulto_m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso_m.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
