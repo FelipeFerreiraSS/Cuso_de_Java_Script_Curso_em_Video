41c65aa2fe3ae4dd05fe42ee63d553dbd4fbeb6b
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-11-01 18:17:32 -0300

    portifolioFinal

--- Java_Script_Curso_em_video/aula14ex/ex017/script.js
+++ Java_Script_Curso_em_video/aula14ex/ex017/script.js
@@ -8,8 +8,9 @@ function tabuada() {
         let c = 1
         tab.innerHTML = ''
         while (c <= 10) {
+            let d = n*c  // para caldular a divisão
             let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
+            item.text = `${n} x ${c} = ${n*c} ▮ ${n} + ${c} = ${n+c} ▮ ${n} - ${c} = ${n-c} ▮ ${n*c} ÷ ${n} = ${d/n}`
             item.value = `tab${c}`
             tab.appendChild(item)
             c++
